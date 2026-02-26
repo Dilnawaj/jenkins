6b96adbeaf6521918e6778c501274e3389456ed8
Dilnawaj Khan <48027940+Dilnawaj@users.noreply.github.com>
2026-02-26 11:51:15 +0530

    jenkins

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main {
+
+      public static void main(String[] args) {
+        System.out.println("Hello");
+        System.err.println("Current date" + new Date());
+    }
+    
+    
+}
