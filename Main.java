706a7c653f9c5b3ec217fdf04916f00cc8416759
Dilnawaj Khan <48027940+Dilnawaj@users.noreply.github.com>
2026-02-26 11:59:01 +0530

    #2

--- Main.java
+++ Main.java
@@ -5,8 +5,9 @@ public class Main {
       public static void main(String[] args) {
         System.out.println("Hello");
         System.err.println("Current date" + new Date());
-        
+
     }
+    //
     
     
 }
