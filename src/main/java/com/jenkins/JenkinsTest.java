65c86232717e5fd76c550d38cdd8c540b44d5960
Dilnawaj Khan <48027940+Dilnawaj@users.noreply.github.com>
2026-04-27 16:29:50 +0530

    added spring boot app

---
+++ src/main/java/com/jenkins/JenkinsTest.java
@@ -0,0 +1,23 @@
+package com.jenkins;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class JenkinsTest {
+
+    @GetMapping
+    public ResponseEntity<String> testJenkins()
+    {
+        return ResponseEntity.ok("Java running successfully.");
+    }
+    @GetMapping("jenkins")
+    public ResponseEntity<String> testJenkinsApi()
+    {
+        return ResponseEntity.ok("Jenkins running successfully.");
+    }
+
+}
